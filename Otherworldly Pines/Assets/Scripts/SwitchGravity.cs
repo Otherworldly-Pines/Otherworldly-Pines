31159671e99f1c4a32d96cc4f552bdf8bbf39fa6
Guanlin Huang <gxh178@case.edu>
2020-09-20 23:45:53 -0400

    flip specific object done
    Moved Switch Gravity to a sub_object of player called "gravity_field"
    
    All objects within the field that has a tag "flippable" to it will be flipped

--- Otherworldly Pines/Assets/Scripts/SwitchGravity.cs
+++ Otherworldly Pines/Assets/Scripts/SwitchGravity.cs
@@ -10,14 +10,21 @@ public class SwitchGravity : MonoBehaviour
 
     private Rigidbody2D rb;
     private int angle = 0;
-
+    private List<Rigidbody2D> flippables = new List<Rigidbody2D>();
     void Start(){
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponentInParent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.J)){
+        FlipGravity();
+        
+    }
+
+    void FlipGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
             angle = angle + 180;
             transform.eulerAngles = new Vector3(0, 0, angle);
             Vector3 Scaler = transform.localScale;
@@ -25,9 +32,36 @@ public class SwitchGravity : MonoBehaviour
             transform.localScale = Scaler;
 
             rb.gravityScale *= -1;
+            if (flippables.Count != 0)
+            {
+                foreach (Rigidbody2D flippable in flippables)
+                {
+                    flippable.gravityScale *= -1;
+                }
+            }
         }
+    }
+
+    void UpdateFlippables()
+    {
         
     }
 
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Rigidbody2D gameObject = collider.GetComponent<Rigidbody2D>();
+        if (collider.gameObject.tag == "flippable" && !flippables.Contains(gameObject))
+        {
+            flippables.Add(gameObject);
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Rigidbody2D gameObject = collider.GetComponent<Rigidbody2D>();
+        if (flippables.Contains(gameObject))
+        {
+            flippables.Remove(gameObject);
+        }
+    }
 }
