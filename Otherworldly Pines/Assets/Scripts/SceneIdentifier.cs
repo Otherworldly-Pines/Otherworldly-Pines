768fa1c376068818754a12e9c5d8182f257001de
Lucas Popp <lmp122@case.edu>
2020-09-22 11:12:31 -0400

    Use strings to identify scenes, and rename Starter Page to Main Menu

---
+++ Otherworldly Pines/Assets/Scripts/SceneIdentifier.cs
@@ -0,0 +1,10 @@
+public class SceneIdentifier
+{
+
+    public static string MainMenu = "Main Menu";
+    public static string Instructions = "Instructions Page";
+    public static string Options = "Options Page";
+    public static string Credits = "Credits Page";
+    public static string Level1 = "SampleScene";
+
+}
