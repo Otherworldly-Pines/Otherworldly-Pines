7d5b6bdd33e80b60f6cf255bf13d57d4a9b89132
Lucas Popp <lmp122@case.edu>
2020-09-22 12:11:02 -0400

    Add GravityControl, GravityRegion, and GravityFlippable

---
+++ Otherworldly Pines/Assets/Scripts/Gravity Flip/GravityControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityControl : MonoBehaviour
+{
+
+    private GravityRegion activeGravityRegion;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnterGravityRegion(GravityRegion region)
+    {
+        this.activeGravityRegion = region;
+    }
+
+    public void ExitGravityRegion(GravityRegion region)
+    {
+        if (activeGravityRegion.gameObject.GetInstanceId() == region.gameObject.GetInstanceId())
+        {
+            this.activeGravityRegion = null;
+        }
+    }
+}
---
+++ Otherworldly Pines/Assets/Scripts/Gravity Flip/GravityFlippable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFlippable : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // Flip gravity
+    public void Flip()
+    {
+        body.gravityScale *= -1;
+    }
+}
