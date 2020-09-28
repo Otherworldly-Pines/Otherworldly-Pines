5ab4e1bb03e0ca395a3854f753796f40309a47a2
Guanlin Huang <gxh178@case.edu>
2020-09-28 16:12:12 -0400

    added a prefab--Thorn_with_placeholder
    Now, the thorn will auto reset the scene if the player touches it. The function can be easily changed to HP loss and stuff.

---
+++ Otherworldly Pines/Assets/Scripts/Thorn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Thorn : MonoBehaviour
+{ 
+    private Scene currentScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+       currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //calls when the player hits the thorn; can be changed to other functions
+    //now, it just assumes player dies instantly, and resets the secne
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
