9cfa118b78edf63224187d3d9852b2a1cd241c9e
Lucas Popp <lmp122@case.edu>
2020-09-28 15:35:02 -0400

    Add gravity flip indicator text

--- Otherworldly Pines/Assets/Scripts/Gravity Flip/GravityControl.cs
+++ Otherworldly Pines/Assets/Scripts/Gravity Flip/GravityControl.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityControl : GravityAffected
-{
+public class GravityControl : GravityAffected {
 
+    public GravityFlipIndicator indicator;
     private GravityRegion activeGravityRegion;
-    
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) && activeGravityRegion != null)
@@ -15,16 +15,19 @@ public class GravityControl : GravityAffected
         }
     }
 
-    public void EnterGravityRegion(GravityRegion region)
-    {
+    private void SetActiveGravityRegion(GravityRegion region) {
         activeGravityRegion = region;
+        indicator.GravityRegionChanged(activeGravityRegion);
+    }
+
+    public void EnterGravityRegion(GravityRegion region) {
+        SetActiveGravityRegion(region);
     }
 
     public void ExitGravityRegion(GravityRegion region)
     {
-        if (activeGravityRegion.gameObject.GetInstanceID() == region.gameObject.GetInstanceID())
-        {
-            activeGravityRegion = null;
+        if (activeGravityRegion.gameObject.GetInstanceID() == region.gameObject.GetInstanceID()) {
+            SetActiveGravityRegion(null);
         }
     }
 }
---
+++ Otherworldly Pines/Assets/Scripts/HUD/GravityFlipIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GravityFlipIndicator : MonoBehaviour {
+
+    public Text text;
+    public GravityControl controller;
+
+    public void GravityRegionChanged(GravityRegion activeGravityRegion) {
+        if (activeGravityRegion == null || !activeGravityRegion.playerCanFlipGravity) {
+            DisplayText("Disabled");
+        } else if (activeGravityRegion.playerCanFlipGravity) {
+            DisplayText("Enabled");
+        }
+    }
+
+    private void DisplayText(string message) {
+        text.text = "Gravity Flip: " + message;
+    }
+
+}
