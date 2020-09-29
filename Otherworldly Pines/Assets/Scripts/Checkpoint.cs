54d41f8c667a9f69faa22b923dd862714ec47c1b
Adam Stewart <adamjoesph11@gmail.com>
2020-09-29 10:24:53 -0400

    adding base scripts for checkpoints

---
+++ Otherworldly Pines/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private CheckpointMaster cm;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cm.lastCheckPointPos = transform.position;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm = GameObject.FindGameObjectWithTag("CM").GetComponent<CheckpointMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Otherworldly Pines/Assets/Scripts/Player/PlayerPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPos : MonoBehaviour
+{
+    private CheckpointMaster cm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm = GameObject.FindGameObjectWithTag("CM").GetComponent<CheckpointMaster>();
+        transform.position = cm.lastCheckPointPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
