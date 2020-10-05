284e84287e35dfa5512d90079e2c81f23222d3ca
Lucas Popp <lmp122@case.edu>
2020-10-04 20:03:48 -0400

    Implement basic freezing

---
+++ Otherworldly Pines/Assets/Scripts/Freezable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freezable : MonoBehaviour {
+
+    private PlayerFreeze player;
+    private Rigidbody2D body;
+    private RigidbodyType2D originalBodyType;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        originalBodyType = body.bodyType;
+
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerFreeze>();
+    }
+
+    public void Freeze() {
+        body.bodyType = RigidbodyType2D.Static;
+    }
+
+    public void Unfreeze() {
+        body.bodyType = originalBodyType;
+        body.velocity = Vector2.zero;
+    }
+
+    private void OnMouseDown() {
+        player.FreezeObject(this);
+    }
+
+    private void OnMouseEnter() {
+        player.StartHovering(this);
+    }
+
+    private void OnMouseExit() {
+        player.StopHovering(this);
+    }
+
+}
---
+++ Otherworldly Pines/Assets/Scripts/Player/PlayerFreeze.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFreeze : MonoBehaviour {
+
+    private static float MAX_TIMEOUT = 10f;
+    
+    private HashSet<Freezable> hoveredObjects = new HashSet<Freezable>();
+    private Freezable frozen;
+    public bool hasFrozen = false;
+    private float freezeTimeout = 0f;
+
+    private void Update() {
+        if (hasFrozen) {
+            freezeTimeout -= Time.deltaTime;
+            if (freezeTimeout < 0) {
+                hasFrozen = false;
+                frozen.Unfreeze();
+            }
+        }
+    }
+
+    public bool IsHoveringAny() {
+        return hoveredObjects.Count > 0;
+    }
+
+    public void StartHovering(Freezable obj) {
+        hoveredObjects.Add(obj);
+    }
+
+    public void StopHovering(Freezable obj) {
+        hoveredObjects.Remove(obj);
+    }
+
+    public void FreezeObject(Freezable obj) {
+        hoveredObjects.Clear();
+        obj.Freeze();
+        frozen = obj;
+        hasFrozen = true;
+        freezeTimeout = MAX_TIMEOUT;
+    }
+
+}
