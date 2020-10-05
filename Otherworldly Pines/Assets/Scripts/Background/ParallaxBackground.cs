f5428a25d272e49299c9c24f456a0d35ec9db654
QingQing Sun <yxs629@case.edu>
2020-10-05 03:02:49 -0400

    added background prefabs and scripts for parallax movement

---
+++ Otherworldly Pines/Assets/Scripts/Background/ParallaxBackground.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ParallaxBackground : MonoBehaviour
+{
+
+    // camera object assigned in inspector
+    [SerializeField] private GameObject cam;
+
+    // bg movement ratio, value 0-1, assigned in inspector
+    // 0 = none; 1 = foreground (following player movespd), usually too fast
+    [SerializeField] private float bgMoveSpeed;
+
+    // placing obj offsetY distance above center of cam
+    [SerializeField] private float offsetY;
+
+    private float move; // parallax movement value
+    private Vector2 startPos;
+    private float objLength, objHeight;
+    private float screenHeightInUnits, screenWidthInUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        objLength = GetComponent<SpriteRenderer>().bounds.size.x;
+        objHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        screenHeightInUnits = Camera.main.orthographicSize * 2;
+        screenWidthInUnits = screenHeightInUnits * Screen.width / Screen.height;
+
+        move = cam.transform.position.x * bgMoveSpeed;
+
+        // moves obj
+        transform.position = new Vector2(startPos.x + move, cam.transform.position.y + offsetY);
+
+        // if object is too far left or right (is offscreen), moves it back over
+        // creates the "scrolling background"
+        if (transform.position.x - cam.transform.position.x < -1.5 * screenWidthInUnits)
+        {
+            transform.position = new Vector2(transform.position.x + (2 * objLength), transform.position.y);
+        }
+        else if (transform.position.x - cam.transform.position.x > 1.5 * screenWidthInUnits)
+        {
+            transform.position = new Vector2(transform.position.x - (2 * objLength), transform.position.y);
+        }
+    }
+}
---
+++ Otherworldly Pines/Assets/Scripts/Background/SkyBackground.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBackground : MonoBehaviour
+{
+    // camera object, assigned in inspector
+    [SerializeField] GameObject cam;
+
+    private SpriteRenderer spriteRenderer;
+    private float camWidth, camHeight;
+    private Vector2 spriteSize;
+
+    // Start is called before the first frame update
+    // ideally we are not resizing the screen during gameplay or it starts at fullscreen (max size)
+    // scales the sky background image to fit the width of the camera while maintaining proportion
+    void Start()
+    {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        camHeight = Camera.main.orthographicSize * 2;
+        camWidth = camHeight * Camera.main.aspect;
+        spriteSize = spriteRenderer.sprite.bounds.size;
+
+        Vector2 bgScale = transform.localScale;
+        if (camWidth >= camHeight)
+        { // Landscape (or equal)
+            bgScale *= camWidth / spriteSize.x;
+        }
+        else
+        { // Portrait
+            bgScale *= camHeight / spriteSize.y;
+        }
+
+        transform.localScale = bgScale;
+
+    }
+}
