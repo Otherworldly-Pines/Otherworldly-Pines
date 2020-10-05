6c14a7578051703387e9a336c705fec69f6cd501
wz9314 <61978931+wz9314@users.noreply.github.com>
2020-10-06 04:34:20 +0800

    add idle state to spirit

--- Otherworldly Pines/Assets/Scripts/PlayerFollow.cs
+++ Otherworldly Pines/Assets/Scripts/PlayerFollow.cs
@@ -7,19 +7,36 @@ public class PlayerFollow : MonoBehaviour
     public float speed;
     public GameObject player;
 
+    public float frequency;
+    public float magnitude;
+    public float stoppingDistX;
+    public float stoppingDistY;
+
     private Transform playerTransform;
+    private Rigidbody2D playerRigidBody;
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = player.GetComponent<Transform>();
+        playerRigidBody = player.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //set where the spirit should be with regards to player
-        Vector2 newPosition = new Vector2(playerTransform.position.x - 2f, playerTransform.position.y + 3f);
-        //follow the player 
-        transform.position = Vector2.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+        //Moving
+        if(playerRigidBody.velocity.magnitude > 0){
+            Vector2 newPosition = new Vector2(playerTransform.position.x - stoppingDistX, playerTransform.position.y + stoppingDistY);
+
+            transform.position = Vector2.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+        }
+        //Idle
+        else{
+            Vector2 newPosition = new Vector2(playerTransform.position.x - stoppingDistX, playerTransform.position.y * Mathf.Sin(Time.time * frequency)*magnitude);
+
+            transform.position = Vector2.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+
+        }
     }
+
 }
