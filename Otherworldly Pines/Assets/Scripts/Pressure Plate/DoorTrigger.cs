ac5371ad5260642557437531051089ec75693170
wz9314 <61978931+wz9314@users.noreply.github.com>
2020-10-07 05:16:10 +0800

    Pressure plate & door (#60)
    * Add basic functionality of the pressure plate and the door
    
    * Make Door and Pressure Plate as Prefab

---
+++ Otherworldly Pines/Assets/Scripts/Pressure Plate/DoorTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+
+    bool hasTriggered = false;
+
+    void OnTriggerEnter2D(Collider2D collider){
+
+        if(!hasTriggered){
+            door.transform.position = new Vector2(door.transform.position.x, door.transform.position.y - door.transform.localScale.y);
+            hasTriggered = true;
+        }
+        
+
+    }
+
+}
