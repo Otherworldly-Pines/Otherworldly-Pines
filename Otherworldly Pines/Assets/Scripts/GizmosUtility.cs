64179053df82c0493f80b0862d8ec6d0b031e7cb
Lucas Popp <lmp122@case.edu>
2020-10-13 12:30:40 -0400

    Validate gravity regions for easier editing (#79)
    * Format the file
    
    * Add GizmosUtility for easier gizmos drawing
    
    * Simplify gizmos
    
    * Add validation to gravity regions

---
+++ Otherworldly Pines/Assets/Scripts/GizmosUtility.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GizmosUtility {
+
+    public static void DrawCollider(Collider2D colliderToShow) {
+        DrawBox(colliderToShow.bounds.center, colliderToShow.bounds.size);
+    }
+
+    public static void DrawBox(Vector2 center, Vector2 size) {
+        Gizmos.DrawRay(center - size / 2f, new Vector2(size.x, 0f));
+        Gizmos.DrawRay(center - size / 2f, new Vector2(0f, size.y));
+        Gizmos.DrawRay(center + size / 2f, new Vector2(-size.x, 0f));
+        Gizmos.DrawRay(center + size / 2f, new Vector2(0f, -size.y));
+    }
+
+}
--- Otherworldly Pines/Assets/Scripts/Gravity Flip/GravityRegion.cs
+++ Otherworldly Pines/Assets/Scripts/Gravity Flip/GravityRegion.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
 
-public class GravityRegion : MonoBehaviour
-{
+public class GravityRegion : MonoBehaviour {
 
     public bool gravityIsFlipped = false;
     public bool playerCanFlipGravity = true;
@@ -18,57 +17,48 @@ public class GravityRegion : MonoBehaviour
     private Color gravityColor = new Color(0f, 0.12f, 0.34f, 1f);
     private int currentFlipCount = 0;
 
-    void Start()
-    {
+    void Start() {
         ownCollider = GetComponent<BoxCollider2D>();
-        if (background != null)
-        {
+        if (background != null) {
             Vector3 updatedScale = new Vector3(ownCollider.size.x, ownCollider.size.y, 1f);
             background.transform.localScale = updatedScale;
             background.transform.localPosition = new Vector3(ownCollider.offset.x, ownCollider.offset.y, 0f);
         }
     }
 
-    void Update()
-    {
-        if (background != null)
-        {
+    void Update() {
+        if (background != null) {
             background.color = gravityIsFlipped ? gravityColor : Color.clear;
         }
     }
 
-    public void FlipGravity()
-    {
+    public void FlipGravity() {
         if (!playerCanFlipGravity) return;
         if (maxFlipCount > 0 && currentFlipCount >= maxFlipCount) return;
 
         currentFlipCount++;
         gravityIsFlipped = !gravityIsFlipped;
-        
+
         flippables.RemoveWhere(flippable => !flippable.StillExists());
         foreach (GravityFlippable flippable in flippables) {
             flippable.Flip();
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
+    private void OnTriggerEnter2D(Collider2D collider) {
         GravityCenter gravityCenter = collider.GetComponent<GravityCenter>();
         if (gravityCenter == null) return;
 
-        foreach (GravityAffected owner in gravityCenter.owners)
-        {
+        foreach (GravityAffected owner in gravityCenter.owners) {
             // If the player just entered the region, inform them of that so they can control it
-            if (owner is GravityControl)
-            {
+            if (owner is GravityControl) {
                 GravityControl controller = owner as GravityControl;
                 controller.EnterGravityRegion(this);
             }
 
             // If a flippable object just entered, add them to this gravity region
             // and if they need their gravity updated, take care of that too
-            if (owner is GravityFlippable)
-            {
+            if (owner is GravityFlippable) {
                 GravityFlippable flippable = owner as GravityFlippable;
                 flippables.Add(flippable);
                 if (gravityIsFlipped != flippable.isUpsideDown) flippable.Flip();
@@ -76,23 +66,19 @@ public class GravityRegion : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collider)
-    {
+    private void OnTriggerExit2D(Collider2D collider) {
         GravityCenter gravityCenter = collider.GetComponent<GravityCenter>();
         if (gravityCenter == null) return;
 
-        foreach (GravityAffected owner in gravityCenter.owners)
-        {
+        foreach (GravityAffected owner in gravityCenter.owners) {
             // If the player just exited, inform them of that
-            if (owner is GravityControl)
-            {
+            if (owner is GravityControl) {
                 GravityControl controller = owner as GravityControl;
                 controller.ExitGravityRegion(this);
             }
 
             // If a flippable object just exited, remove them from the list of flippabless
-            if (owner is GravityFlippable)
-            {
+            if (owner is GravityFlippable) {
                 GravityFlippable flippable = owner as GravityFlippable;
                 flippables.Remove(flippable);
             }
@@ -102,19 +88,29 @@ public class GravityRegion : MonoBehaviour
     private void OnDrawGizmos() {
         if (!ownCollider) ownCollider = GetComponent<BoxCollider2D>();
         Gizmos.color = Color.blue;
-        Gizmos.DrawLine(ownCollider.bounds.min, new Vector3(ownCollider.bounds.min.x, ownCollider.bounds.max.y, 0f));
-        Gizmos.DrawLine(ownCollider.bounds.min, new Vector3(ownCollider.bounds.max.x, ownCollider.bounds.min.y, 0f));
-        Gizmos.DrawLine(ownCollider.bounds.max, new Vector3(ownCollider.bounds.min.x, ownCollider.bounds.max.y, 0f));
-        Gizmos.DrawLine(ownCollider.bounds.max, new Vector3(ownCollider.bounds.max.x, ownCollider.bounds.min.y, 0f));
+        GizmosUtility.DrawCollider(ownCollider);
     }
 
     private void OnDrawGizmosSelected() {
         if (!ownCollider) ownCollider = GetComponent<BoxCollider2D>();
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(ownCollider.bounds.min, new Vector3(ownCollider.bounds.min.x, ownCollider.bounds.max.y, 0f));
-        Gizmos.DrawLine(ownCollider.bounds.min, new Vector3(ownCollider.bounds.max.x, ownCollider.bounds.min.y, 0f));
-        Gizmos.DrawLine(ownCollider.bounds.max, new Vector3(ownCollider.bounds.min.x, ownCollider.bounds.max.y, 0f));
-        Gizmos.DrawLine(ownCollider.bounds.max, new Vector3(ownCollider.bounds.max.x, ownCollider.bounds.min.y, 0f));
+        GizmosUtility.DrawCollider(ownCollider);
+    }
+
+    private Vector3 Snap(Vector3 original) {
+        return new Vector3(Mathf.Round(original.x * 4f) / 4f, Mathf.Round(original.y * 4f) / 4f, Mathf.Round(original.z * 4f) / 4f);
+    }
+    
+    private void OnValidate() {
+        if (!ownCollider) ownCollider = GetComponent<BoxCollider2D>();
+        if (ownCollider.offset.magnitude > 0.001f) {
+            Vector2 newPosition = transform.position + (Vector3)ownCollider.offset;
+            transform.position = newPosition;
+            ownCollider.offset = Vector2.zero;
+        }
+
+        transform.position = Snap(transform.position);
+        ownCollider.size = Snap(ownCollider.size);
     }
 
 }
