604b349c90fe70e2b313bfa4346549330ddf568e
Chen Nie <70956754+Chen-Nie@users.noreply.github.com>
2020-10-14 18:08:28 -0400

    Added collectibles in Demo level (#70)
    * Added collectibles in Demo level
    
    Still need HUDs
    
    * updating collectable prefabs and giving them their own folder
    
    Co-authored-by: Domnee <70956754+Domnee@users.noreply.github.com>
    Co-authored-by: QQ <67279766+s1q2y@users.noreply.github.com>
    Co-authored-by: Adam Stewart <adamjoesph11@gmail.com>

---
+++ Otherworldly Pines/Assets/Scripts/Collectibles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public int c1 = 2;
+    public int c2 = 1;
+    private int c1Collected = 0; // num of C1 collected
+    private int c2Collected = 0; // num of C2 collected
+
+    void Update()
+    {
+        if (c1 == c1Collected)
+        {
+            // show all C1 are collected
+        }
+
+        if (c2 == c2Collected)
+        {
+            // show all C2 are collected
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.CompareTag("Collectible1"))
+        {
+            Destroy(target.gameObject);
+            c1Collected++;
+            // show c1Collected/c1 are collected
+        }
+        else if (target.gameObject.CompareTag("Collectible2"))
+        {
+            Destroy(target.gameObject);
+            c2Collected++;
+            // show c2Collected/c2 are collected
+        }
+    }
+}
