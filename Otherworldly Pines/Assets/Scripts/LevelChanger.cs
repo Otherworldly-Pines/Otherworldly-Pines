95960771e2174488d1f739ad940a4c1f76ddc937
grace-yufei <61996284+grace-yufei@users.noreply.github.com>
2020-10-18 15:02:21 -0400

    fade scripts added

---
+++ Otherworldly Pines/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class LevelChanger : MonoBehaviour
+{
+    public Animator fade_animator;
+    public float transitionTime = 1f;
+    public string levelName;
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(LoadLevel());
+        }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        fade_animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelName);
+    }
+}
