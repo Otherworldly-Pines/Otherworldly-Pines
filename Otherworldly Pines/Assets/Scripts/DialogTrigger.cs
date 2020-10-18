586a3f35b4d3c39a4f9dd7f6430916d083b25b2d
Lucas Popp <lmp122@case.edu>
2020-10-18 12:48:29 -0400

    Trigger dialogues by walking into them, and add the tutorial text to level 1

---
+++ Otherworldly Pines/Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public TextMeshProUGUI dialogText;
+    public GameObject dialogDisplay;
+    public GameObject continueText;
+    public string[] sentences;
+  
+    public float charTypingInterval;
+
+    private int sentenceIndex;
+
+    private bool isDisplaying = false;
+    private GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        var playerControls = other.gameObject.GetComponent<PlayerControls>();
+        if (playerControls != null) {
+            player = other.gameObject;
+            
+            isDisplaying = true;
+
+            var gravityControls = player.GetComponent<GravityControl>();
+            var throwControls = player.GetComponent<PlayerThrow>();
+            playerControls.FreezeControls();
+            gravityControls.FreezeControls();
+            throwControls.FreezeControls();
+
+            dialogDisplay.SetActive(true);
+            continueText.SetActive(false);
+            dialogText.text = "";
+            StartCoroutine(TypeText());
+        }
+    }
+
+    private void Update() {
+        if (isDisplaying) {
+            if (dialogText.text == sentences[sentenceIndex] && Input.anyKeyDown) {
+                if (sentenceIndex < sentences.Length - 1) {
+                    NextSentence();
+                } else {
+                    dialogDisplay.SetActive(false);
+                    
+                    var playerControls = player.GetComponent<PlayerControls>();
+                    var gravityControls = player.GetComponent<GravityControl>();
+                    var throwControls = player.GetComponent<PlayerThrow>();
+                    
+                    playerControls.UnfreezeControls();
+                    gravityControls.UnfreezeControls();
+                    throwControls.UnfreezeControls();
+
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach(char letter in sentences[sentenceIndex].ToCharArray()){
+            dialogText.text += letter;
+            yield return new WaitForSeconds(charTypingInterval);
+        }
+
+        continueText.SetActive(true);
+    }
+
+    public void NextSentence() {
+        sentenceIndex++;
+        dialogText.text = "";
+        StartCoroutine(TypeText());
+        continueText.SetActive(false);
+    }
+}
