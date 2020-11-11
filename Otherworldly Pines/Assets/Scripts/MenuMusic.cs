ceb7421a175e407729c58ca66f81371142a7122e
Guanlin Huang <gxh178@case.edu>
2020-11-11 13:20:22 -0500

    Added menu music and button click sound

---
+++ Otherworldly Pines/Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    private AudioSource menuAudioSource;
+    private AudioSource buttonClickSource;
+    [SerializeField] private AudioClip buttonClick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var clickAudioObj = new GameObject();
+        menuAudioSource = GetComponent<AudioSource>();
+        buttonClickSource = clickAudioObj.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        menuAudioSource.volume = GameSettings.musicVolume;
+        buttonClickSource.volume = GameSettings.sfxVolume;
+    }
+
+    public void playButtonClick()
+    {
+        buttonClickSource.PlayOneShot(buttonClick);
+    }
+}
